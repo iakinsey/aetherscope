d8ec57bbb9fab79016427f3a3e3dcaac7ef517be
Ian Kinsey <ian@aikbix.com>
2025-12-22 21:19:02 -0600

    Complete fs object store tests

--- src/services/object_store/fs.rs
+++ src/services/object_store/fs.rs
@@ -1,7 +1,7 @@
 use std::{io::ErrorKind, path::PathBuf};
 
 use async_trait::async_trait;
-use tokio::fs::{create_dir_all, remove_file, try_exists, write};
+use tokio::fs::{create_dir_all, read, remove_file, try_exists, write};
 
 use crate::types::{error::AppError, traits::object_store::ObjectStore};
 
@@ -20,7 +20,7 @@ impl FileSystemObjectStore {
 #[async_trait]
 impl ObjectStore for FileSystemObjectStore {
     async fn get(&self, key: &str) -> Result<Vec<u8>, AppError> {
-        unimplemented!();
+        Ok(read(self.path.join(key)).await?)
     }
     async fn put(&self, key: &str, data: &[u8]) -> Result<(), AppError> {
         Ok(write(self.path.join(key), data).await?)
@@ -34,5 +34,37 @@ impl ObjectStore for FileSystemObjectStore {
     }
     async fn exists(&self, key: &str) -> Result<bool, AppError> {
         Ok(try_exists(self.path.join(key)).await?)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use uuid::Uuid;
+
+    use super::*;
+    use std::env::temp_dir;
+
+    #[tokio::test]
+    async fn test_fs_object_store_crud() {
+        let path = temp_dir().join(Uuid::new_v4().to_string());
+        let contents = "Hello world!".as_bytes();
+        let key = "test_key";
+        let store = FileSystemObjectStore::new(path).await.unwrap();
+
+        assert_eq!(
+            store.get(key).await.unwrap_err().to_string(),
+            "No such file or directory (os error 2)"
+        );
+        store.put(key, contents).await.unwrap();
+        let contents = store.get(key).await.unwrap();
+
+        assert_eq!(contents, contents);
+
+        store.delete(key).await.unwrap();
+
+        assert_eq!(
+            store.get(key).await.unwrap_err().to_string(),
+            "No such file or directory (os error 2)"
+        );
     }
 }
