8417548f1511b410ff4b993f4ba466dc1bea1644
Ian Kinsey <ian@aikbix.com>
2026-01-14 00:24:52 -0600

    Frontier manager boilerplate

--- src/tasks/frontier_manager.rs
+++ src/tasks/frontier_manager.rs
@@ -1,4 +1,28 @@
-// TODO configurable options for these attributes
-// - Uniqueness filter (bloom filter?)
-// - PageRank
-// - Robots filter
+use async_trait::async_trait;
+
+use crate::types::{
+    configs::frontier_manager_config::FrontierManagerConfig, error::AppError,
+    structs::record::Record, traits::task::Task,
+};
+
+pub struct FrontierManager<'a> {
+    config: &'a FrontierManagerConfig,
+}
+
+impl<'a> FrontierManager<'a> {
+    pub async fn new(config: &'a FrontierManagerConfig) -> Result<Self, AppError> {
+        Ok(Self { config })
+    }
+}
+
+#[async_trait]
+impl<'a> Task for FrontierManager<'a> {
+    async fn on_message(&self, message: Record) -> Result<Record, AppError> {
+        // TODO configurable options for these attributes
+        // - Uniqueness filter (bloom filter?)
+        // - PageRank
+        // - Robots filter
+
+        unimplemented!()
+    }
+}
--- src/types/configs/mod.rs
+++ src/types/configs/mod.rs
@@ -1,3 +1,4 @@
+pub mod frontier_manager_config;
 pub mod headless_browser;
 pub mod http_fetcher_config;
 pub mod url_extractor_config;
