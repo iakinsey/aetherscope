7ccbeb58d59768356596e962c0defca47c169b2b
Ian Kinsey <ian@aikbix.com>
2026-02-08 00:52:55 -0600

    Some tests pass

--- src/utils/fsm/title_fsm.rs
+++ src/utils/fsm/title_fsm.rs
@@ -9,7 +9,8 @@ pub enum TitleParseState {
     ReadTitle,
     Terminate,
 }
-
+// TODO terminate if you see the end of head
+// TODO terminate if you exceed a number of bytes, somewhere in the 4kb range
 static TITLE: OnceLock<Vec<char>> = OnceLock::new();
 fn title() -> &'static [char] {
     TITLE.get_or_init(|| vec!['t', 'i', 't', 'l', 'e', '>'])
@@ -77,11 +78,7 @@ impl TitleExtractorFSM {
     }
 
     async fn read_html_tag(&mut self) -> Result<(), AppError> {
-        if !self.reader.match_next(&['a'], true).await? {
-            return Ok(());
-        }
-
-        if !self.reader.read_until_match(title(), '<', true).await? {
+        if self.reader.read_until_match(title(), '<', true).await? {
             self.state = TitleParseState::ReadTitle;
         }
 
@@ -92,5 +89,107 @@ impl TitleExtractorFSM {
         self.title = self.reader.get_until_term(tag_term()).await?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+
+    use super::*;
+    use crate::types::traits::object_store::AsyncReadSeek;
+    use std::io::Cursor;
+
+    fn reader_from_static_str(s: &'static str) -> Box<dyn AsyncReadSeek + Send + Unpin + 'static> {
+        Box::new(Cursor::new(s.as_bytes()))
+    }
+
+    #[tokio::test]
+    async fn extracts_simple_title() {
+        let html = r#"<html><head><title>Hello World</title></head></html>"#;
+        let fsm = TitleExtractorFSM::new(reader_from_static_str(html)).unwrap();
+        let title = fsm.perform().await.unwrap();
+
+        assert_eq!("Hello World", title);
+    }
+
+    #[tokio::test]
+    async fn ignores_content_before_title() {
+        let html = r#"
+        garbage garbage
+        <div>noise</div>
+        <title>Actual Title</title>
+    "#;
+
+        let fsm = TitleExtractorFSM::new(reader_from_static_str(html)).unwrap();
+        let title = fsm.perform().await.unwrap();
+
+        assert_eq!("Actual Title", title);
+    }
+
+    #[tokio::test]
+    async fn handles_empty_title() {
+        let html = r#"<title></title>"#;
+        let fsm = TitleExtractorFSM::new(reader_from_static_str(html)).unwrap();
+        let title = fsm.perform().await.unwrap();
+
+        assert_eq!("", title);
+    }
+
+    #[tokio::test]
+    async fn stops_at_first_title() {
+        let html = r#"
+        <title>First</title>
+        <title>Second</title>
+    "#;
+
+        let fsm = TitleExtractorFSM::new(reader_from_static_str(html)).unwrap();
+        let title = fsm.perform().await.unwrap();
+
+        assert_eq!("First", title);
+    }
+
+    #[tokio::test]
+    async fn eof_without_title_returns_empty() {
+        let html = r#"<html><body>No title here</body></html>"#;
+
+        let fsm = TitleExtractorFSM::new(reader_from_static_str(html)).unwrap();
+        let title = fsm.perform().await.unwrap();
+
+        assert_eq!("", title);
+    }
+
+    #[tokio::test]
+    async fn title_with_special_chars() {
+        let html = r#"<title>Rust &amp; Tokio <Test></title>"#;
+
+        let fsm = TitleExtractorFSM::new(reader_from_static_str(html)).unwrap();
+        let title = fsm.perform().await.unwrap();
+
+        assert_eq!("Rust &amp; Tokio ", title);
+    }
+
+    #[tokio::test]
+    async fn whitespace_preserved_inside_title() {
+        let html = r#"<title>   spaced   out   </title>"#;
+
+        let fsm = TitleExtractorFSM::new(reader_from_static_str(html)).unwrap();
+        let title = fsm.perform().await.unwrap();
+
+        assert_eq!("   spaced   out   ", title);
+    }
+
+    #[tokio::test]
+    async fn title_after_multiple_tags() {
+        let html = r#"
+        <meta name="x">
+        <link rel="y">
+        <title>Deep Title</title>
+    "#;
+
+        let fsm = TitleExtractorFSM::new(reader_from_static_str(html)).unwrap();
+        let title = fsm.perform().await.unwrap();
+
+        assert_eq!("Deep Title", title);
     }
 }
