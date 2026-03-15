8ff66bffdcc74f6b4e1db1b3330cdaab5b87ea59
Ian Kinsey <ian@aikbix.com>
2026-03-14 23:48:46 -0500

    Signal scorer basics

--- src/tasks/signal_scorer.rs
+++ src/tasks/signal_scorer.rs
@@ -1,12 +1,21 @@
 use async_trait::async_trait;
 
 use crate::types::{
+    configs::tasks::signal_scorer_config::SignalScorerConfig,
     error::AppError,
     structs::{metadata::execution_context::ExecutionContext, record::Record},
     traits::task::Task,
 };
 
-pub struct SignalScorer {}
+pub struct SignalScorer {
+    config: SignalScorerConfig,
+}
+
+impl SignalScorer {
+    pub async fn new(config: SignalScorerConfig) -> Result<Self, AppError> {
+        unimplemented!()
+    }
+}
 
 #[async_trait]
 impl Task for SignalScorer {
--- src/types/configs/tasks/mod.rs
+++ src/types/configs/tasks/mod.rs
@@ -2,4 +2,5 @@ pub mod frontier_manager_config;
 pub mod headless_browser_config;
 pub mod http_fetcher_config;
 pub mod signal_extractor_config;
+pub mod signal_scorer_config;
 pub mod url_extractor_config;
